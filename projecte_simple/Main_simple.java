0ab54c25aa72dc674bcc176a398c10df6d89d4c9
KuraiShinigami <dcollado98@gmail.com>
2022-05-27 09:25:30 +0200

    Create Main_simple.java

---
+++ projecte_simple/Main_simple.java
@@ -0,0 +1,10 @@
+public class projecte_simple {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+        System.out.println("hello world 2");
+        System.out.println("hello world 3");
+
+    }
+}
